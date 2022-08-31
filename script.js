22deb8b3d16cb5d91c207c503808351f27dbd944
Samson Cheung <samsonc89@yahoo.com>
2022-08-31 14:17:52 -0700

    Show message when passwords do not match

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 
 const pwd = document.querySelector("#password");
 const confirmPwd = document.querySelector("#confirm_password");
+const matchMsg = document.querySelector("#matchMsg");
 
-confirmPwd.addEventListener("input", () => {
-  console.log("Hello");
+confirmPwd.addEventListener("focusout", () => {
+  if (pwd.value !== confirmPwd.value) {
+    confirmPwd.setCustomValidity("Passwords must match");
+    matchMsg.classList.remove("hidden");
+  } else {
+    confirmPwd.setCustomValidity("");
+    matchMsg.classList.add("hidden");
+  }
 });
