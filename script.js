5a521f7b1cff1c5cbee54ea4d2c39d48c2fdb06c
Samson Cheung <samsonc89@yahoo.com>
2022-09-01 13:09:34 -0700

    Add error message for password format

--- script.js
+++ script.js
@@ -5,11 +5,33 @@ const confirmPwd = document.querySelector("#confirm_password");
 const matchMsg = document.querySelector("#matchMsg");
 
 confirmPwd.addEventListener("focusout", () => {
-  if (pwd.value !== confirmPwd.value) {
-    confirmPwd.setCustomValidity("Passwords must match");
-    matchMsg.classList.remove("hidden");
+  if (confirmPwd.checkValidity() == false) {
+    if (document.querySelector(".validPwd") == null) {
+      confirmPwd.insertAdjacentHTML(
+        "afterend",
+        `    <p class="validPwd">
+      Password must be <br />
+      At least 6 characters<br />
+      At least 1 uppercase character<br />
+      At least 1 lowercase character<br />
+      At least 1 number
+    </p>`
+      );
+    }
   } else {
-    confirmPwd.setCustomValidity("");
-    matchMsg.classList.add("hidden");
+    if (pwd.value !== confirmPwd.value) {
+      document.querySelector(".validPwd").remove();
+      confirmPwd.setCustomValidity("Passwords must match");
+      matchMsg.classList.remove("hidden");
+    } else {
+      confirmPwd.setCustomValidity("");
+      matchMsg.classList.add("hidden");
+    }
+  }
+});
+
+pwd.addEventListener("focusout", () => {
+  if (pwd.checkValidity() == false) {
+    console.log("please type correct pwd");
   }
 });
